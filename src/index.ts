62137afc3a841643f430bd9bddf145661c3d002a
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-08-18 13:56:37 +0530

    chore: init

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { createPublicClient, http } from 'viem'
+import { mainnet } from 'viem/chains'
+
+const client = createPublicClient({
+  chain: mainnet,
+  transport: http(),
+})
+
+
+const test = async () => {
+    const blockNumber = await client.getBlockNumber();
+    console.log(`Block number: ${blockNumber}`);
+}
+
+test();
+
+console.log('Shit!')
