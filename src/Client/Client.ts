f57497500aacaf94af5852ef954014b2d6bd7f1d
Jaxcoder <codenamejason@outlook.com>
2023-12-05 14:28:26 -0500

    example

--- src/Client/Client.ts
+++ src/Client/Client.ts
@@ -1,6 +1,18 @@
 import { Chain, PublicClient, Transport, createPublicClient, http } from "viem";
 
-
+/**
+ * 
+ * Returns a PublicClient instance that can be used to interact with the Allo v2 contracts.
+ * 
+ * @remarks
+ * This is a wrapper around the viem PublicClient class that sets the correct chain ID and transport.
+ * 
+ * @param chain - The chain ID to connect to (this is a Chain type from viem)
+ * @param rpc - The url of the RPC endpoint
+ * @returns - A PublicClient instance
+ * 
+ * @beta
+ */
 export const create = (chain: Chain, rpc?: string): PublicClient<Transport, Chain> => {
   const client = createPublicClient({
     chain: chain,
