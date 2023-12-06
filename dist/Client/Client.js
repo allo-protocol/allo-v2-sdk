b8d89f19def051361a32f51dd108652d7c992396
Jaxcoder <codenamejason@outlook.com>
2023-12-06 10:44:43 -0500

    add script for docs

--- dist/Client/Client.js
+++ dist/Client/Client.js
@@ -2,6 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.create = void 0;
 const viem_1 = require("viem");
+/**
+ *
+ * Returns a PublicClient instance that can be used to interact with the Allo v2 contracts.
+ *
+ * @remarks
+ * This is a wrapper around the viem PublicClient class that sets the correct chain ID and transport.
+ *
+ * @param chain - The chain ID to connect to (this is a Chain type from viem)
+ * @param rpc - The url of the RPC endpoint
+ * @returns - A PublicClient instance
+ *
+ * @beta
+ */
 const create = (chain, rpc) => {
     const client = (0, viem_1.createPublicClient)({
         chain: chain,
