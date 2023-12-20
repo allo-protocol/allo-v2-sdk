f78adaa722753e4688b8d371811b917ec9dd00ed
0xKurt <Kurt@Merbeth.io>
2023-12-20 14:09:54 +0100

    adding dummy tes

--- dist/__tests__/Registry/Registry.test.d.ts
+++
@@ -1,2 +0,0 @@
-import { Metadata } from "../../Common/types";
-export declare const metadata: Metadata;
--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,10 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testPathIgnorePatterns: [
+    "<rootDir>/dist/__tests__/",
     "<rootDir>/src/__tests__/utils/",
     "<rootDir>/dist/__tests__/utils/",
+    "<rootDir>/src/__tests__ignore/",
     "<rootDir>/dist/__tests__ignore/",
   ],
 };
