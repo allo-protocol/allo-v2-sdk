28bda1193d71c7020fe3c5ce237e28fdcd123605
0xKurt <Kurt@Merbeth.io>
2024-01-05 11:29:20 +0100

    skip tests

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,11 @@ module.exports = {
   testPathIgnorePatterns: [
     "<rootDir>/src/__tests__/utils/",
     "<rootDir>/dist/__tests__/utils/",
+    "<rootDir>/src/__tests__/Allo",
+    "<rootDir>/dist/__tests__/Allo",
+    "<rootDir>/src/__tests__/Registry",
+    "<rootDir>/dist/__tests__/Registry",
+    "<rootDir>/src/__tests__/Strategy",
+    "<rootDir>/dist/__tests__/Strategy",
   ],
 };
