350eb63fe63eebef1711c739590ed0c98ad8e5ca
0xKurt <Kurt@Merbeth.io>
2024-02-07 14:12:29 +0100

    exports and updates

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,20 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.SQFSuperFluidStrategy = exports.MicroGrantsStrategy = exports.Registry = exports.Allo = void 0;
+exports.DonationVotingMerkleDistributionStrategy = exports.SQFSuperFluidStrategy = exports.MicroGrantsStrategy = exports.Registry = exports.Allo = exports.SQFSuperFluidStrategyAbi = exports.MicroGrantsStrategyAbi = exports.DonationVotingMerkleDistributionVaultStrategyAbi = exports.DonationVotingMerkleDistributionDirectTransferStrategyAbi = exports.RegistryAbi = exports.AlloAbi = exports.SQFSuperFluidStrategyTypes = exports.MicroGrantsStrategyTypes = exports.DonationVotingMerkleDistributionStrategyTypes = void 0;
 const Allo_1 = require("./Allo/Allo");
 Object.defineProperty(exports, "Allo", { enumerable: true, get: function () { return Allo_1.Allo; } });
 const Registry_1 = require("./Registry/Registry");
@@ -9,3 +23,24 @@ const MicroGrantsStrategy_1 = require("./strategies/MicroGrantsStrategy/MicroGra
 Object.defineProperty(exports, "MicroGrantsStrategy", { enumerable: true, get: function () { return MicroGrantsStrategy_1.MicroGrantsStrategy; } });
 const SQFSuperFluidStrategy_1 = require("./strategies/SuperFluidStrategy/SQFSuperFluidStrategy");
 Object.defineProperty(exports, "SQFSuperFluidStrategy", { enumerable: true, get: function () { return SQFSuperFluidStrategy_1.SQFSuperFluidStrategy; } });
+const DonationVotingMerkleDistribution_1 = require("./strategies/DonationVotingMerkleDistributionStrategy/DonationVotingMerkleDistribution");
+Object.defineProperty(exports, "DonationVotingMerkleDistributionStrategy", { enumerable: true, get: function () { return DonationVotingMerkleDistribution_1.DonationVotingMerkleDistributionStrategy; } });
+__exportStar(require("./types"), exports);
+__exportStar(require("./strategies/types"), exports);
+__exportStar(require("./Registry/types"), exports);
+__exportStar(require("./Allo/types"), exports);
+exports.DonationVotingMerkleDistributionStrategyTypes = require("./strategies/DonationVotingMerkleDistributionStrategy/types");
+exports.MicroGrantsStrategyTypes = require("./strategies/MicroGrantsStrategy/types");
+exports.SQFSuperFluidStrategyTypes = require("./strategies/SuperFluidStrategy/types");
+var allo_config_1 = require("./Allo/allo.config");
+Object.defineProperty(exports, "AlloAbi", { enumerable: true, get: function () { return allo_config_1.abi; } });
+var registry_config_1 = require("./Registry/registry.config");
+Object.defineProperty(exports, "RegistryAbi", { enumerable: true, get: function () { return registry_config_1.abi; } });
+var donationVotingDirect_config_1 = require("./strategies/DonationVotingMerkleDistributionStrategy/donationVotingDirect.config");
+Object.defineProperty(exports, "DonationVotingMerkleDistributionDirectTransferStrategyAbi", { enumerable: true, get: function () { return donationVotingDirect_config_1.abi; } });
+var donationVotingVault_config_1 = require("./strategies/DonationVotingMerkleDistributionStrategy/donationVotingVault.config");
+Object.defineProperty(exports, "DonationVotingMerkleDistributionVaultStrategyAbi", { enumerable: true, get: function () { return donationVotingVault_config_1.abi; } });
+var microGrants_config_1 = require("./strategies/MicroGrantsStrategy/microGrants.config");
+Object.defineProperty(exports, "MicroGrantsStrategyAbi", { enumerable: true, get: function () { return microGrants_config_1.abi; } });
+var superfluid_config_1 = require("./strategies/SuperFluidStrategy/superfluid.config");
+Object.defineProperty(exports, "SQFSuperFluidStrategyAbi", { enumerable: true, get: function () { return superfluid_config_1.abi; } });
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,27 @@ import { Allo } from "./Allo/Allo";
 import { Registry } from "./Registry/Registry";
 import { MicroGrantsStrategy } from "./strategies/MicroGrantsStrategy/MicroGrantsStrategy";
 import { SQFSuperFluidStrategy } from "./strategies/SuperFluidStrategy/SQFSuperFluidStrategy";
+import { DonationVotingMerkleDistributionStrategy } from "./strategies/DonationVotingMerkleDistributionStrategy/DonationVotingMerkleDistribution";
 
-export { Allo, Registry, MicroGrantsStrategy, SQFSuperFluidStrategy };
+export * from "./types";
+export * from "./strategies/types";
+export * from "./Registry/types";
+export * from "./Allo/types";
+export * as DonationVotingMerkleDistributionStrategyTypes from "./strategies/DonationVotingMerkleDistributionStrategy/types";
+export * as MicroGrantsStrategyTypes from "./strategies/MicroGrantsStrategy/types";
+export * as SQFSuperFluidStrategyTypes from "./strategies/SuperFluidStrategy/types";
+
+export { abi as AlloAbi } from "./Allo/allo.config";
+export { abi as RegistryAbi } from "./Registry/registry.config";
+export { abi as DonationVotingMerkleDistributionDirectTransferStrategyAbi } from "./strategies/DonationVotingMerkleDistributionStrategy/donationVotingDirect.config";
+export { abi as DonationVotingMerkleDistributionVaultStrategyAbi } from "./strategies/DonationVotingMerkleDistributionStrategy/donationVotingVault.config";
+export { abi as MicroGrantsStrategyAbi } from "./strategies/MicroGrantsStrategy/microGrants.config";
+export { abi as SQFSuperFluidStrategyAbi } from "./strategies/SuperFluidStrategy/superfluid.config";
+
+export {
+  Allo,
+  Registry,
+  MicroGrantsStrategy,
+  SQFSuperFluidStrategy,
+  DonationVotingMerkleDistributionStrategy,
+};
