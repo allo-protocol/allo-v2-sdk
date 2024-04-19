acd251141138f2b33b1afa061abf921bf2e58a53
0xKurt <Kurt@Merbeth.io>
2024-04-19 14:33:12 +0200

    rm unused

--- src/strategies/DirectGrantsLiteStrategy/types.ts
+++ src/strategies/DirectGrantsLiteStrategy/types.ts
@@ -1,10 +1,5 @@
 import { Metadata } from "../../Common/types";
 
-export type ApplicationStatus = {
-  index: number;
-  statusRow: number; // todo: Big number?
-};
-
 export type Recipient = {
   useRegistryAnchor: boolean;
   recipientAddress: `0x${string}`;
@@ -28,11 +23,4 @@ export type Allocation = {
   token: `0x${string}`;
   recipientId: `0x${string}`;
   amount: bigint;
-};
-
-export type Claim = { recipientId: `0x${string}`; token: `0x${string}` };
-
-export const StrategyType = {
-  Vault: "Vault",
-  Direct: "Direct",
 };
