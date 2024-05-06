251c2eaf4fca0e37e076d5c6188c0a16565de66a
Aditya Anand M C <aditya.anandmc@gmail.com>
2024-05-06 16:08:48 +0530

    bump version

--- dist/__tests__/dummy.test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-describe("dummy test", () => {
-    it("should pass", () => {
-        expect(true).toBe(true);
-    });
-});
--- dist/__tests__/utils/utils.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const makeAddress: (input: string) => string;
-export declare const makeBytes32: (input: string) => string;
-export declare const NATIVE: () => string;
