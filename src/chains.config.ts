2fda146d33477c470c3360e9729d6fb170849ff0
0xKurt <Kurt@Merbeth.io>
2024-05-29 12:22:30 +0200

    add sei custom chain

--- src/chains.config.ts
+++ src/chains.config.ts
@@ -1,4 +1,4 @@
-import { Chain } from "viem";
+import { Chain, defineChain } from "viem";
 import {
   arbitrum,
   arbitrumSepolia,
@@ -24,6 +24,29 @@ import {
   zkSyncSepoliaTestnet,
 } from "viem/chains";
 
+export const sei = /*#__PURE__*/ defineChain({
+  id: 1329,
+  name: "Sei",
+  nativeCurrency: { name: "Sei", symbol: "SEI", decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ["https://evm-rpc.sei-apis.com/"],
+      webSocket: ["wss://evm-ws.sei-apis.com/"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Seitrace",
+      url: "https://seitrace.com",
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: "0xcb2436774C3e191c85056d248EF4260ce5f27A9D",
+    },
+  },
+});
+
 export const supportedChains: Chain[] = [
   arbitrum,
   arbitrumSepolia,
@@ -43,6 +66,7 @@ export const supportedChains: Chain[] = [
   fantom,
   scroll,
   seiDevnet,
+  sei,
   avalancheFuji,
   fantomTestnet,
   zkSync,
